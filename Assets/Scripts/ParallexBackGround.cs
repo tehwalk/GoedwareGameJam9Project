025c1b0843fa1a8b7fc1d232d78cadb346f55eb9
SimonianMossi <92452093+SimonianMossi@users.noreply.github.com>
2023-09-24 20:01:14 +0300

    Έκανα το BG να κινείται σε 2 layers

---
+++ Assets/Scripts/ParallexBackGround.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallexBackGround : MonoBehaviour
+{
+    [SerializeField] private float parallaxEffectMultiplier;
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+
+    private void Start() {
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+    }
+
+    private void LateUpdate() {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += deltaMovement * parallaxEffectMultiplier;
+        lastCameraPosition = cameraTransform.position;
+    }
+}
