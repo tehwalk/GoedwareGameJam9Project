0961f21d64ee5a9c72a336a4d82e57fa34eb856e
tehwalk <eiraloupi2@gmail.com>
2023-10-01 15:42:53 +0300

    1-10-23 Commit
    -added 1st cutscene
    -changed player graphics

---
+++ Assets/Scripts/CutsceneManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class CutsceneManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI speechText;
+    [TextArea(3, 10)]
+    [SerializeField] private string[] speechParts;
+    [SerializeField] private float typeTime;
+    [SerializeField] private SceneIndex nextScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TypeText());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator TypeText()
+    {
+        for (int i = 0; i < speechParts.Length; i++)
+        {
+            foreach (char c in speechParts[i].ToCharArray())
+            {
+                speechText.text += c;
+                yield return new WaitForSeconds(typeTime);
+            }
+            yield return new WaitForSeconds(typeTime * 2.5f);
+            speechText.text += "\n";
+        }
+        SceneManager.LoadScene((int)nextScene);
+    }
+}
--- Assets/Scripts/SceneIndexComponent.cs
+++ Assets/Scripts/SceneIndexComponent.cs
@@ -5,11 +5,16 @@ using UnityEngine.SceneManagement;
 public enum SceneIndex
 {
     MainMenu = 0,
-    Level1 = 1,
-    Level2 = 2,
-    Level3 = 3,
-    Level4 = 4,
-    Level5 = 5
+    Cutscene1 = 1,
+    Level1 = 2,
+    Cutscene2= 3,
+    Level2 = 4,
+    Cutscene3 = 5,
+    Level3 = 6,
+    Cutscene4 = 7,
+    Level4 = 8,
+    Cutscene5 = 9,
+    Level5 = 10
 }
 
 public class SceneIndexComponent : MonoBehaviour
